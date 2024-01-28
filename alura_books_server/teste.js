5e43d67dac9391a6d62ef0b9c372251b783ccd4f
Marlon Menezes <marlonmnz@Marlons-iMac-Pro.local>
2024-01-28 14:52:23 -0300

    Feat: Feito teste de leitura e escrita do arquivo JSON

--- alura_books_server/teste.js
+++ alura_books_server/teste.js
@@ -1,3 +1,9 @@
-const fs = require('fs');
+const fs = require("fs");
 
-console.log(JSON.parse(fs.readFileSync('livros.json')));
+const dadosAtuais = JSON.parse(fs.readFileSync("livros.json"));
+
+const novoDado = { id: 3, nome: "Livro 3" };
+
+fs.writeFileSync("livros.json", JSON.stringify([...dadosAtuais, novoDado]));
+
+console.log(JSON.parse(fs.readFileSync("livros.json")));
