ec9d5ceae510288527db74d7afd0c0ee052babc3
Marlon <marlon_menezes@outlook.com>
2024-01-29 11:32:59 -0300

    Fix: Ajuste nos controladores para resolver problema do Express

--- alura_books_server/app.js
+++ alura_books_server/app.js
@@ -1,21 +1,13 @@
-// Aqui é onde a aplicação é iniciada e onde se encontra a configuração inicial do servidor. Estamos utilizando o express para criar o servidor. O express é um framework para Node.js que facilita a criação de aplicações web e APIs. O express é um middleware que possui diversos métodos que facilitam a criação de aplicações web e APIs.
+const express = require('express');
 
-// Aqui estamos importando o express:
-const express = require("express");
+const rotaLivro = require('./rotas/livro');
 
-// Aqui estamos importando as rotas:
-const rotaLivro = require("./rotas/livro").default;
-
-// Aqui estamos criando o servidor:
 const app = express();
 
-// Aqui estamos definindo a porta que o servidor irá rodar:
-const port = process.env.PORT || 8000;
+const port = 8000;
 
-// O método 'use' é responsável por adicionar um middleware ao servidor.
-app.use("/livros", rotaLivro);
+app.use('/livros', rotaLivro);
 
-// O método 'listen' é responsável por iniciar o servidor e mostrar que ele está rodando na porta 8000 que nós informamos acima.
 app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
--- alura_books_server/controladores/livro.js
+++ alura_books_server/controladores/livro.js
@@ -1,16 +1,14 @@
-// Os controladores são responsáveis por receber as requisições HTTP, processar os dados e enviar uma resposta ao cliente. Os controladores são chamados pelas rotas, que são responsáveis por receber as requisições HTTP e chamar os controladores, tirando assim a responsabilidade das rotas de processar os dados e enviar uma resposta ao cliente. Todas as respostas seja de erro ou sucesso devem ser enviadas pelos controladores.
-
-const fs = require("fs")
+const { getTodosLivros } = require('../servicos/livro');
 
 function getLivros(req, res) {
   try {
-    const livros = JSON.parse(fs.readFileSync("livros.json"))
-    res.send(livros)
-  } catch (error) {
-    res.status(500).send({ error: 'Internal server error' });
+      const livros = getTodosLivros();
+      res.send(livros);
+  } catch (err) {
+      res.status(500).send({error: err.message});
   }
 }
 
 module.exports = {
-  getLivros,
+  getLivros
 };
